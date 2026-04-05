21a0ae3ac220f51fdd87169ce300247bf36cb1c3
yusa-a <yusaaihara3@gmail.com>
2026-04-05 18:27:27 +0900

    add test

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -4,7 +4,7 @@
 int main( void ) {
 	Fixed a;
 
-	// <<8で整数に -> roundf -> *ではtoFloat() -> <<でtoFloat() -> std::cout有効数字6桁のため四捨五入
+	 <<8で整数に -> roundf -> *ではtoFloat() -> <<でtoFloat() -> std::cout有効数字6桁のため四捨五入
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
 	std::cout << a << std::endl;
@@ -14,6 +14,16 @@ int main( void ) {
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
+
+	// fn
+	//Fixed a2(2000);
+	//Fixed b2(5000);
+	//Fixed result = a2 * b2;
+
+	//std::cout << a2 << std::endl;
+	//std::cout << b2 << std::endl;
+	//std::cout << result << std::endl;
+
 	return 0;
 }
 
