9e10f38b68b907dba4f4096ba217bfda76b11faa
yusa-a <yusaaihara3@gmail.com>
2026-08-30 10:23:30 +0900

    fix: 01

--- Module09/ex01/main.cpp
+++ Module09/ex01/main.cpp
@@ -1,10 +1,10 @@
-J#include <iostream>
+#include <iostream>
 
 #include "RPN.hpp"
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        std::cout << "Usage: ./RPN \"expression\"" << std::endl;
+        std::cerr << "Usage: ./RPN \"expression\"" << std::endl;
         return 1;
     }
 
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
         RPN calculator;
         std::cout << calculator.evaluate(argv[1]) << std::endl;
     } catch (const std::exception& exception) {
-        std::cout << "Error: " << exception.what() << std::endl;
+        std::cerr << "Error: " << exception.what() << std::endl;
         return 1;
     }
     return 0;
